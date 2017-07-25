3b5a478280acf2c9a7f2110d30868506884c3455
sharmanirudh <sharmanirudh27@gmail.com>
2017-07-25 16:49:55 +0530

    AJAX: new Q/A & upvote/downvote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,49 +1,84 @@
-class HomeController < ActionController::Base
+class HomeController < ApplicationController
 
 	def index
 	end
 
 	def question
 		if(params["content"] != "")
-			question = Question.new
-			question.content = params["content"]
-			question.user_id = current_user.id
-			question.save
+			@question = Question.new
+			@question.content = params["content"]
+			@question.user_id = current_user.id
+			if @question.save
+				respond_to do |format|
+					format.js {    }
+				end
+			else
+
+			end
 		end
-		return redirect_to '/'
 	end
 	def answer
 		if(params["content"] != "")
-			answer = Answer.new
-			answer.content = params["content"]
-			answer.user_id = current_user.id
-			answer.question_id = params["question_id"]
-			answer.save
+			@answer = Answer.new
+			@answer.content = params["content"]
+			@answer.user_id = current_user.id
+			@question_id = params["question_id"]
+			@answer.question_id = @question_id
+			if @answer.save
+				respond_to do |format|
+					format.js {    }
+				end
+			else
+
+			end
 		end
-		return redirect_to '/'
 	end
 	def up_vote
-		upvotetype_id = params["upvotetype_id"]
-		upvote_type = params["type"]
-		if( !Upvote.where(upvotetype_id: upvotetype_id, upvote_type: upvote_type, user_id: current_user).first )
-			upvote = Upvote.new
-			upvote.upvotetype_id = upvotetype_id
-			upvote.upvote_type = upvote_type
-			upvote.user_id = current_user.id
-			upvote.save
+		@type_id = params["upvotetype_id"]
+		@type = params["type"]
+		if(earlier_downvote = Downvote.where(downvotetype_id: @type_id, downvote_type: @type, user_id: current_user).first)
+			earlier_downvote.destroy
+			respond_to do |format|
+				format.js {  render "down_vote.js.erb"  }#if already existing down vote is there then update its count
+			end
+		else
+			if( !Upvote.where(upvotetype_id: @type_id, upvote_type: @type, user_id: current_user).first )
+				upvote = Upvote.new
+				upvote.upvotetype_id = @type_id
+				upvote.upvote_type = @type
+				upvote.user_id = current_user.id
+				if upvote.save
+					respond_to do |format|
+						format.js {    }
+					end
+				else
+
+				end
+			end
 		end
-		return redirect_to '/'
 	end
 	def down_vote
-		downvotetype_id = params["downvotetype_id"]
-		downvote_type = params["type"]
-		if( !Downvote.where(downvotetype_id: downvotetype_id, downvote_type: downvote_type, user_id: current_user).first )
-			downvote = Downvote.new
-			downvote.downvotetype_id = downvotetype_id
-			downvote.downvote_type = downvote_type
-			downvote.user_id = current_user.id
-			downvote.save
+		@type_id = params["downvotetype_id"]
+		@type = params["type"]
+		if(earlier_upvote = Upvote.where(upvotetype_id: @type_id, upvote_type: @type, user_id: current_user).first)
+			earlier_upvote.destroy
+			respond_to do |format|
+				format.js {  render "up_vote.js.erb"  }# if already existing up vote is there then update its count
+			end
+		else
+			if( !Downvote.where(downvotetype_id: @type_id, downvote_type: @type, user_id: current_user).first )
+				downvote = Downvote.new
+				downvote.downvotetype_id = @type_id
+				downvote.downvote_type = @type
+				downvote.user_id = current_user.id
+				if downvote.save
+					respond_to do |format|
+						format.js {    }
+					end
+				else
+
+				end
+			end
 		end
-		return redirect_to '/'
 	end
 end
